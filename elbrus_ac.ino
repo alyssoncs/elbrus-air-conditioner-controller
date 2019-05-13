318b254a0bc01a655f2218e59bf00af3ba4d30e4
alyssoncs <alysson.cirilo@gmail.com>
2019-05-13 12:43:43 -0300

    moved delay() inside the for() loop

--- elbrus_ac.ino
+++ elbrus_ac.ino
@@ -12,15 +12,15 @@ void setup() {
  * iterate through all the supported temperatures
  */
 void loop() {
-
+  int khz = 38;
+  
   for (int i = MIN_TEMP; i <= MAX_TEMP; i++) {
-    int khz = 38;
     byte temperatureBuff[MESSAGE_SIZE]; /* MESSAGE_SIZE is defined in elbrus_ac.h */
     
     getTemperatureRepresentation(temperatureBuff, MESSAGE_SIZE, i);
     sendRawByBit(irsend, temperatureBuff, khz);
+    
+    delay(DELAY_TIME);
   }
-
-  delay(DELAY_TIME);
 }
 
